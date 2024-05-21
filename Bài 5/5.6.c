8748c13721e064be832e9d6ae9febc0049540760
MrTadano <90030454+MrTadano@users.noreply.github.com>
2024-05-21 21:37:53 +0700

    Update 5.6.c

--- Bài 5/5.6.c
+++ Bài 5/5.6.c
@@ -1,1 +1,31 @@
-Code chưa tối ưu để làm sau
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char s[99],t[99],resstr[30];
+	int restime[30];
+    scanf("%s%s",s,t);
+    int reslen = 0;
+    for(char i = 'a';i<'z'+1;i++){
+    	int hook=0;
+		for(int j=0;j<strlen(s);j++){
+			if(i==s[j]){
+				hook+=1;
+			}
+		}
+		for(int k=0;k<strlen(t);k++){
+			if(i==t[k]){
+				hook+=1;
+			}
+		}
+		if(hook>0){
+			resstr[reslen]=i;
+			restime[reslen]=hook;
+			reslen++;
+		}
+	}
+	printf("%s",resstr);
+	for(int i=0;i<reslen;i++){
+		printf("\n%d",restime[i]);
+	}
+    return 0;
+}
