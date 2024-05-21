a85f3f7b75a6aa2de32f93e878ab4f9102044c3b
MrTadano <90030454+MrTadano@users.noreply.github.com>
2024-05-21 19:17:57 +0700

    Update 4.3.c

--- Bài 4/4.3.c
+++ Bài 4/4.3.c
@@ -5,19 +5,11 @@ int sumn(int num)
 {   
     int a;
     int b=0;
-    int n=num;
-    int len=0;
-    while (n != 0)
-    {
-        len++;
-        n /= 10;
-    }
-    while (len!=0)
+    while (num!=0)
     {
         a=num%10;
         b=b+a;
         num /=10;
-        len--;
     }
     return b;
 }
@@ -26,7 +18,6 @@ int main() {
 
     int n ;
     scanf("%d",&n);
-    char num[50];
     if(n>=1000) 
         printf("ERROR");
     else
