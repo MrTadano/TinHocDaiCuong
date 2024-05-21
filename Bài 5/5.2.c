eb2eea5cc300955057230b83785de001b4312a8a
MrTadano <90030454+MrTadano@users.noreply.github.com>
2024-05-21 19:42:12 +0700

    Update 5.2.c

--- Bài 5/5.2.c
+++ Bài 5/5.2.c
@@ -3,8 +3,6 @@
 int main() {
     int n;
     scanf("%d", &n);
-    if(n<=0) printf("Error");
-    else{
     int ns[n]; 
     int i; double a=0; int as=0; double d=0;
     for(i=0;i<n;i++)
@@ -20,6 +18,5 @@ int main() {
     else printf("Mang khong co so am ");
     if(as==n) printf("Mang khong co so duong");
     else printf("%lf",d);
-    }
     return 0;
 }
